3c494ee2e2c826569be1064793710fbfbf7d8d17
antoniobrau <brauantonio21@gmail.com>
2025-10-19 21:52:01 +0200

    problema nell'uso di extern da c++17

--- include/GlobalSettings.hpp
+++ include/GlobalSettings.hpp
@@ -1,64 +1,44 @@
-#ifndef COSTANTSETTINGS_HPP
-#define COSTANTSETTINGS_HPP
-
+#pragma once
 #include <fstream>
-#include <sstream>
 #include <string>
-#include <unordered_map>
-
 
 constexpr int SPACE_FILTER = 3;
-constexpr int  TIME_FILTER = 3;
+constexpr int TIME_FILTER  = 3;
 constexpr size_t DIM_MULTI_LEVEL = 1;
 
-extern double PROMOTION_PROBABILITY = 0.999;
-extern size_t MAX_ITEMS = 100000;
-extern int WIDTH = 768;
-extern int HEIGHT = 576;
-extern int POOL_SIZE = 10;
-extern size_t max_items = 100000;
-extern int max_window_length = -1;
-extern std::string DIRECTORY_VIDEO = "";
-extern std::string DIRECTORY_OUT = "";
+// variabili globali (una sola definizione grazie a 'inline')
+inline double PROMOTION_PROBABILITY = 0.999;
+inline size_t MAX_ITEMS = 100000;
+inline int WIDTH = 768;
+inline int HEIGHT = 576;
+inline int POOL_SIZE = 10;
+inline int max_window_length = -1;
+inline std::string DIRECTORY_VIDEO{};
+inline std::string DIRECTORY_OUT{};
 
-
-void load_config(const std::string& path) {
+// funzione in header: deve essere inline
+inline void load_config(const std::string& path) {
     std::ifstream f(path);
     if (!f) return;
-    std::string key; double val; std::string sval;
+    std::string key;
     while (f >> key) {
-        if (key ==  "DIRECTORY_VIDEO"){
-            f >> sval;
-            DIRECTORY_VIDEO = sval;
-        }
-        else if (key == "DIRECTORY_OUT"){
-            f >> sval;
-            DIRECTORY_OUT = sval;
-        }
-        else{
-            f >> val;
-            if (key == "WIDTH") WIDTH = static_cast<int>(val);
-            else if ( key == "HEIGHT") HEIGHT = static_cast<int>(val);
-            else if (key == "POOL_SIZE") POOL_SIZE = static_cast<int>(val);
-            else if (key == "PROMOTION_PROBABILITY") PROMOTION_PROBABILITY = val;
-            else if (key == "MAX_ITEMS") MAX_ITEMS = static_cast<size_t>(val);
-            else if (key == "MAX_WINDOW_LENGTH") max_window_length = static_cast<int>(val);
-        }
+        if (key == "DIRECTORY_VIDEO")      f >> DIRECTORY_VIDEO;
+        else if (key == "DIRECTORY_OUT")   f >> DIRECTORY_OUT;
+        else if (key == "WIDTH")           f >> WIDTH;
+        else if (key == "HEIGHT")          f >> HEIGHT;
+        else if (key == "POOL_SIZE")       f >> POOL_SIZE;
+        else if (key == "PROMOTION_PROBABILITY") f >> PROMOTION_PROBABILITY;
+        else if (key == "MAX_ITEMS")       f >> MAX_ITEMS;
+        else if (key == "MAX_WINDOW_LENGTH") f >> max_window_length;
+        else { std::string dump; f >> dump; } // ignora chiave sconosciuta
     }
 }
 
+// una sola istanza globale che auto-carica la config (grazie a 'inline')
 struct ConfigLoader {
     ConfigLoader() { load_config("config.txt"); }
 };
-static ConfigLoader _auto_load;
-
-#endif // COSTANTSETTINGS_HPP
-
-
-
-
-
-
+inline ConfigLoader _auto_load;
 
 
 
